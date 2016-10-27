2f6e11a877606f469cd5770bf474bb6fd4485e7e
juliasin <juliasinegina@gmail.com>
2016-10-28 00:00:11 +0300

    Project

---
+++ CodeFirstProduct/Models/ProductType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstProduct.Models
+{
+    public class ProductType
+    {
+        [Key]
+        public int TypeID { get; set; }
+        public string TypeName { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
